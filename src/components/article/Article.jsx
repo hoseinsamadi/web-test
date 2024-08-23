96f4969a6173db4239c1c35b08c59cb017c8d156
HoseinBiBak <husein.samadi@gmail.com>
2024-08-23 13:51:37 +0330

    درس 21

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,12 @@
 import styled from "./article.module.css"
+import jsi from "./../../assets/images/js.jpeg"
+
 function Article() {
     return (
         <div className={styled.articlewarpper}>
-           
+           <img src={jsi} />
+           <h3>این یک پروزه ریکت است</h3>
+           <p>این خواندن 5 دقیقه ای است</p>
         </div>
     );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,26 @@ import Article from "../../components/article/Article";
 import Navbar from "../../components/navbar/Navbar"
 import styled from "./home.module.css"
 function Home(){
+    let articles = [
+        {
+            id:1,
+            imageUrl:"",
+            title: "عنوان اول",
+            readingTime: 5
+        },
+        {
+            id:2,
+            imageUrl:"",
+            title: "عنوان دوم",
+            readingTime: 3
+        },
+        {
+            id:3,
+            imageUrl:"",
+            title: "عنوان سوم",
+            readingTime: 8
+        }
+    ];
     return(
         <div className={styled.homeWarpper}>
             <Navbar title = "حسین بلاگ"/>
@@ -10,14 +30,13 @@ function Home(){
                         مقالات پیشنهادی
                     </h2>
                     <div className= {styled.articleW}>
-                    <Article/>
-                    <Article/>
-                    <Article/>
-                    <Article/>
-                    <Article/>
-                    <Article/>
-                    <Article/>
-                    <Article/>
+                        {
+                            articles.map(result => (
+                                <Article/>
+                            ))
+                        }
+                        
+
                     </div>
              </div>
         </div>
